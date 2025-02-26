5316277061f17ccb170cd19882fad8e353aa07d3
Daniela Arvelo <arvelo_n@hotmail.com>
2025-02-25 21:38:02 -0300

    Se modifica lista de amigos por tipo array

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Crear un array vacío para almacenar los nombres de los amigos
+let amigos = [];
+
 // Función para agregar amigo a la lista
 function agregarAmigo() {
     // Obtener el valor ingresado en el input
     const nombreAmigo = document.getElementById('amigo').value;
-    
+
+    // Agregar el nombre al array
+    amigos.push(nombreAmigo);
+
     // Crear un nuevo elemento de lista
     const li = document.createElement('li');
     li.textContent = nombreAmigo;
